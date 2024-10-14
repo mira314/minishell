e4f4cc0296270805bc202e0a4f3d31bd174113b2
RANDRIANOELISOA Valsoa Mirado Narindra <152292628+mira314@users.noreply.github.com>
2024-10-14 14:26:29 +0200

    "pip"

--- minishell/src/parse/pipe_proces.c
+++ minishell/src/parse/pipe_proces.c
@@ -11,3 +11,16 @@
 /* ************************************************************************** */
 
 #include"parse.h"
+
+t_token *parsing_pipe(t_data *data, t_token *token)
+{
+    t_cmd *cmd;
+
+    cmd = data->cmd;
+    while (cmd->next)
+        cmd = cmd->next;
+    cmd->output =  1;
+    cmd = lst_add_back_cmd(cmd, new_cmd());
+    token =  token->next;
+    return (token);
+}
