ec0a282e60eb8ea5faac83cf851f1ad00b81de5b
DiaryLanto <rakotoarilantonirina@gmail.com>
2024-10-16 10:17:04 +0300

    add error printing in mini_cd

--- minishell/src/exec/mini_cd.c
+++ minishell/src/exec/mini_cd.c
@@ -6,7 +6,7 @@
 /*   By: derakoto <derakoto@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 14:25:50 by derakoto          #+#    #+#             */
-/*   Updated: 2024/10/12 14:26:28 by derakoto         ###   ########.fr       */
+/*   Updated: 2024/10/16 10:14:34 by derakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ static int	cd_to_home(void)
 int	mini_cd(char **args)
 {
 	int	arg_len;
+	int	result;
 
 	errno = 0;
 	if (args == NULL)
@@ -34,12 +35,19 @@ int	mini_cd(char **args)
 	arg_len = mini_tbl_len(args);
 	if (arg_len > 2)
 	{
-		print_error(args[0], "too many arguments", 1);
+		print_error(args[0], ": too many arguments", 1);
 		return (1);
 	}
 	if (arg_len < 2)
 		return (cd_to_home());
 	if (ft_strlen(args[1]) == 0)
 		return (cd_to_home());
-	return (chdir(args[1]));
+	result = chdir(args[1]);
+	if (result == -1)
+	{
+		ft_putstr_fd("cd: ",STDERR_FILENO);
+		perror(args[1]);
+		return (1);
+	}
+	return (0);
 }
