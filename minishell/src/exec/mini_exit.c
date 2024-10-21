965e5124e7b62d487e1042f4bcd58f2ee56d1b2d
DiaryLanto <rakotoarilantonirina@gmail.com>
2024-10-21 05:47:47 +0300

    when pressing ctrl-D the minishell crashed => fixed

--- minishell/src/exec/mini_exit.c
+++ minishell/src/exec/mini_exit.c
@@ -6,7 +6,7 @@
 /*   By: derakoto <derakoto@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 05:19:02 by derakoto          #+#    #+#             */
-/*   Updated: 2024/10/21 05:07:44 by derakoto         ###   ########.fr       */
+/*   Updated: 2024/10/21 05:43:21 by derakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void    mini_exit(t_data *data)
 {
     mini_tbl_free(data->env);
     printf("exit\n");
+    if (data->cmd == NULL)
+        exit(0);
     if (mini_tbl_len(data->cmd->args) > 2)
     {
         ft_putstr_fd("exit: too many arguments\n", 2);
