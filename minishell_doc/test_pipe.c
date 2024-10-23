028bdee3ddb33c4b5a7268cfccfb832783b65466
DiaryLanto <rakotoarilantonirina@gmail.com>
2024-10-23 05:50:25 +0300

    finished testing pipe

--- minishell_doc/test_pipe.c
+++ minishell_doc/test_pipe.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
-
-int    pipe_loop(char **str, int i)
+int    pipe_loop(char ***cmds, int i, int *input)
 {
-    int     fd[2];
+    int     output[2];
     int     pid;
     int     print[2];
 
-    if (pipe(fd) == -1)
+    if (pipe(output) == -1)
     {
         perror("piping");
         return (1);
@@ -19,32 +19,57 @@ int    pipe_loop(char **str, int i)
     if (pid == -1)
     {
         perror("Forking");
-        close(fd[1]);
-        close(fd[2]);
+        close(output[1]);
+        close(output[2]);
         return (1);
     }
     if (pid == 0)
     {
-        sleep(i);
-        printf("PROCESS %d : %s\n", i, str[0]);
+        if (input != NULL)
+        {
+            close(input[1]);
+            dup2(input[0], 0);
+            close(input[0]);
+        }
+        close(output[0]);
+        if (cmds[1] != 0)
+            dup2(output[1], 1);
+        close(output[1]);
+        /*if (input != NULL)
+        {
+            readed = read(0, buffer, 500);
+            buffer[readed] = 0;
+            write(1, buffer, strlen(buffer));
+        }*/
+        execve(cmds[0][0], cmds[0], NULL);
         exit(0);
     }
     else
     {
-        if (str[1] != 0)
-            pipe_loop(str + 1, i + 1);
+        if (input != NULL)
+        {
+            close(input[0]);
+            close(input[1]);
+        }
+        if (cmds[1] != 0)
+            pipe_loop(cmds + 1, i + 1, output);
         wait(NULL);
-        write(1,"parent ", 7);
-        print[0] = i + 0x30;
-        write(1, print, 1);
-        write(1, "\n", 1);
     }
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-    char    *str[]={"Bonjour", "les etudiants", "de 42", "Mlay", "Galaxy", "Andraharo",NULL};
+    char    *cmd[]={"/usr/bin/ls", NULL};
+    char    *cmd1[]={"/usr/bin/grep", "fil", NULL};
+    char    *cmd2[]={"/usr/bin/sleep", "2", NULL};
+    char    *cmd3[]={"/usr/bin/cat", "-e", NULL};
+    char    **cmds[5]={NULL, NULL, NULL, NULL, NULL};
 
-    pipe_loop(str, 0);
+    cmd1[1] = argv[1];
+    cmds[0] = cmd2;
+    cmds[1] = cmd;
+    cmds[2] = cmd1;
+    cmds[3] = cmd3;
+    pipe_loop(cmds, 0, NULL);
     return (0);
 }
