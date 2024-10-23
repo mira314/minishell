ac7a963bc5c0769b9c5c890230b3c2cec106c831
DiaryLanto <rakotoarilantonirina@gmail.com>
2024-10-23 06:24:41 +0300

    finished the test of pipe

--- minishell_doc/test_pipe.c
+++ minishell_doc/test_pipe.c
@@ -4,11 +4,40 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int    pipe_loop(char ***cmds, int i, int *input)
+static void close_pipe(int *fds)
+{
+    close(fds[0]);
+    close(fds[1]);
+}
+
+static void fork_fun(int *input, int *output, char ***cmds)
+{
+    if (input != NULL)
+    {
+        close(input[1]);
+        dup2(input[0], 0);
+        close(input[0]);
+    }
+    close(output[0]);
+    if (cmds[1] != 0)
+        dup2(output[1], 1);
+    close(output[1]);
+    execve(cmds[0][0], cmds[0], NULL);
+    exit(5);
+}
+
+static int  on_forking_error_pipe(int *fds)
+{
+    perror("Forking");
+    close_pipe(fds);
+    return (1);
+}   
+int    pipe_loop(char ***cmds, int *input)
 {
     int     output[2];
     int     pid;
     int     print[2];
+    int     status;
 
     if (pipe(output) == -1)
     {
@@ -17,59 +46,36 @@ int    pipe_loop(char ***cmds, int i, int *input)
     }
     pid = fork();
     if (pid == -1)
-    {
-        perror("Forking");
-        close(output[1]);
-        close(output[2]);
-        return (1);
-    }
+        on_forking_error_pipe(output);   
     if (pid == 0)
-    {
-        if (input != NULL)
-        {
-            close(input[1]);
-            dup2(input[0], 0);
-            close(input[0]);
-        }
-        close(output[0]);
-        if (cmds[1] != 0)
-            dup2(output[1], 1);
-        close(output[1]);
-        /*if (input != NULL)
-        {
-            readed = read(0, buffer, 500);
-            buffer[readed] = 0;
-            write(1, buffer, strlen(buffer));
-        }*/
-        execve(cmds[0][0], cmds[0], NULL);
-        exit(0);
-    }
+        fork_fun(input, output, cmds);
     else
     {
         if (input != NULL)
-        {
-            close(input[0]);
-            close(input[1]);
-        }
+            close_pipe(input);
         if (cmds[1] != 0)
-            pipe_loop(cmds + 1, i + 1, output);
-        wait(NULL);
+        {
+            pipe_loop(cmds + 1, output);
+            wait(NULL);
+        }
+        else
+        {
+            wait(&status);
+            if (WIFEXITED(status))
+                printf("Exited with status %d\n", WEXITSTATUS(status));
+            else if(WIFSIGNALED(status))
+                printf("Terminated by signal %d\n", WTERMSIG(status));
+            fflush(stdout);
+        }
     }
 }
 
 int main(int argc, char **argv)
 {
-    char    *cmd[]={"/usr/bin/ls", NULL};
-    char    *cmd1[]={"/usr/bin/grep", "fil", NULL};
-    char    *cmd2[]={"/usr/bin/sleep", "2", NULL};
-    char    *cmd3[]={"/usr/bin/cat", "-e", NULL};
-    char    **cmds[5]={NULL, NULL, NULL, NULL, NULL};
+    char    *cmd0[]={"ping", NULL};
+    char    **cmds[3]={NULL, NULL, NULL};
 
-    cmd1[1] = argv[1];
-    cmds[0] = cmd2;
-    cmds[1] = cmd;
-    cmds[2] = cmd1;
-    cmds[3] = cmd3;
-    pipe_loop(cmds, 0, NULL);
+    cmds[0] = cmd0;
+    pipe_loop(cmds, NULL);
     return (0);
 }
