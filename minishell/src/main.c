46c89c2551dedc84cd51fd8b4b9403b7a5000613
Valisoa Mirado Narindra Randrianoelisoa <vrandria@c1r5s2.42antananarivo.mg>
2024-11-01 10:38:17 +0300

    delele break :

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vrandria <vrandria@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 10:56:40 by vrandria          #+#    #+#             */
-/*   Updated: 2024/10/27 14:29:11 by vrandria         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:37:23 by vrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,7 +152,6 @@ int main(int argc, char *argv[], char **env)
 		else
 			g_last_val = 0;
 		clear_lst_cmd(&data);
-		break ;
 	}
 	clear_tab(data.env);
 	return 0;
