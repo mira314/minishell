b4288d489c9a262066c2c6b6e8d65979b09977c0
DiaryLanto <rakotoarilantonirina@gmail.com>
2024-11-25 19:50:13 +0300

    exec norm is finished

--- minishell/src/exec/exec.h
+++ minishell/src/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: derakoto <derakoto@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 05:51:35 by derakoto          #+#    #+#             */
-/*   Updated: 2024/11/25 18:14:35 by derakoto         ###   ########.fr       */
+/*   Updated: 2024/11/25 19:46:17 by derakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,11 @@
 # include <sys/stat.h>
 # define PWD_MAX_LEN 199
 
-void    exec();
 int		ft_echo(char **args);
-int	    ft_pwd(void);
+int		ft_pwd(void);
 int		mini_tbl_len(char **tbl);
 void	mini_tbl_free(char **tbl);
-int 	mini_cd(t_data *data, t_cmd *cmd);
+int		mini_cd(t_data *data, t_cmd *cmd);
 char	**dup_env(char **f_env);
 char	**add_env(char **o_env, char **n_env);
 int		env_key_len(char *env);
@@ -38,35 +37,35 @@ int		is_key_env_valid(char *env);
 int		is_env_duplicated(char **envs, int index);
 char	**add_one_env(char **o_env, char *env);
 char	**try_update(char **o_env, char *env);
-void    unset_one_env(char **o_env, char *env);
+void	unset_one_env(char **o_env, char *env);
 int		mini_unset(char **o_env, char **env);
 int		is_env_key(char *env, char *str);
-int 	ft_strcmp(const char *str1, const char *str2);
+int		ft_strcmp(const char *str1, const char *str2);
 void	sort_env(char **env);
-int	    export(char **env);
+int		export(char **env);
 void	export_one_env(char *env);
-int	    is_key_of(char *key, char *env);
-int     is_file_in(char *path, char *file_name);
-char    *get_env_value(char **envs, char *key);
+int		is_key_of(char *key, char *env);
+int		is_file_in(char *path, char *file_name);
+char	*get_env_value(char **envs, char *key);
 char	*get_env(char **envs, char *key);
-char    *is_in_path_env(char *file_name, char **envs);
-char    *build_path(char *path, char *file_name);
-int 	exec_one_cmd(t_data *data, t_cmd *cmd);
+char	*is_in_path_env(char *file_name, char **envs);
+char	*build_path(char *path, char *file_name);
+int		exec_one_cmd(t_data *data, t_cmd *cmd);
 int		mini_env(char **args, char **envs);
-int	    mini_export(char **args, char ***envs);
-void    mini_exit(t_data *data, t_cmd *cmd);
+int		mini_export(char **args, char ***envs);
+void	mini_exit(t_data *data, t_cmd *cmd);
 char	**update_env(char **o_env, char *env);
-int	    mini_is_numeric(char *str);
-int 	exec_with_redir(t_data *data, t_cmd *cmd);
-int     is_var_assignement(char *str);
-int     count_assignement(char **tbl);
-int     handle_var(char **tbl, char ***var, int *assign_count);
+int		mini_is_numeric(char *str);
+int		exec_with_redir(t_data *data, t_cmd *cmd);
+int		is_var_assignement(char *str);
+int		count_assignement(char **tbl);
+int		handle_var(char **tbl, char ***var, int *assign_count);
 void	exec(t_data *data);
-int	    is_delimiter(char *delimiter, char *str);
-void    clean_pipes(int **pipes, int size);
-void    free_and_exit(t_data *data, int exit_code);
-int	    handle_redir(t_io_fd *io);
-int	    redir_output(t_output *file);
+int		is_delimiter(char *delimiter, char *str);
+void	clean_pipes(int **pipes, int size);
+void	free_and_exit(t_data *data, int exit_code);
+int		handle_redir(t_io_fd *io);
+int		redir_output(t_output *file);
 int		redir_input(t_input *file);
 int		is_path(char *str);
 int		execute_path(t_data *data, t_cmd *cmd, char *path);
@@ -82,10 +81,11 @@ void	close_unused_pipe(int **pipe_fds, int size, int current_index);
 void	close_one_pipe_fd(int *fds);
 void	close_all_pipes(int **pipe_fds, int size);
 void	clean_pipes(int **pipes, int size);
-int		count_cmd (t_cmd *cmd);
+int		count_cmd(t_cmd *cmd);
 int		**create_pipes(int pipe_count);
 int		init_pipes(int **pipe_fds, int size);
 int		on_forking_error_pipe(int *fds);
-void	fork_and_execute(t_data *data, int **pipe_fds, t_cmd *cmd, int cmd_count);
+void	fork_and_execute(t_data *data, int **pipe_fds,
+			t_cmd *cmd, int cmd_count);
 
 #endif
